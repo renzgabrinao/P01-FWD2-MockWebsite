e8f469818c7f2fb35068fb845e1e913814a83a49
Scott Croin <scroin@my.bcit.ca>
2023-02-09 15:54:49 -0800

    mobile responsive news
    ToDO: add desktop and tablet responsive styling to news slider

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -52,7 +52,7 @@ $(".show-slider").slick({
     // settings: "unslick"
     // instead of a settings object
   ],
-}) (jQuery);
+});
 
 function scrollToTop() {
   window.scrollTo(0, 0);
@@ -71,7 +71,6 @@ var videoPopup = document.getElementById("video-popup");
 playButton.addEventListener("click", function() {
   videoPopup.style.display = "block";
   document.body.style.overflow = "hidden";
-  // video.play();
 });
 
 //Close video
@@ -81,12 +80,51 @@ closeButton.addEventListener("click", function() {
   video.pause();
 });
 
-// //Click outside video
-// videoPopup.addEventListener("click", function() {
-//   videoPopup.style.display = "none";
-//   document.body.style.overflow = "auto";
-//   video.pause();
-// });
-
-
 //End of Video Popup Script
+
+// Start of In the News Slider //
+$(".news-slider").slick({
+  prevArrow: $(".news-prev"),
+  nextArrow: $(".news-next"),
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: true,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: "unslick"
+    },
+  ],
+});
+
+
+$(window).resize(function () {
+  $('.js-slider').not('.slick-initialized').slick('resize');
+});
+
+$(window).on('orientationchange', function () {
+  $('.js-slider').not('.slick-initialized').slick('resize');
+});
+
+
+// End of In the News Slider //
