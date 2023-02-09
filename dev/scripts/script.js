e593da61ae28553cf0b0056e8b8c803726148dae
Mason Barry <mbarry22@my.bcit.ca>
2023-02-09 13:05:15 -0800

    Shop By Room DONE

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -26,26 +26,30 @@ $(".show-slider").slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1300,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         infinite: true,
         dots: false,
       },
     },
+    
     {
-      breakpoint: 600,
+      breakpoint: 800,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 3,
+        slidesToScroll: 3,
       },
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        
+        slidesToShow: 8,
         slidesToScroll: 1,
+        
+
       },
     },
     // You can unslick at a given breakpoint now by adding:
